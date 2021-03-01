86bd03baf6695c40f3b64ff5f05d11a9db09d037
StasyTyapkina <stasya.tyapkina@gmail.com>
2021-03-01 16:33:35 +0300

    feat: add styles for square

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Footer from '../src/components/footer/Footer'
+import Footer from './components/footer/Footer'
+import Square from './components/square/Square';
 
 
 const App = ()=>{
   return (
     <div className = 'app'>
       <h1>Tic-Tac-Toe React Game</h1>
+      <Square/>
       <Footer/>
     </div>
   );
---
+++ src/components/square/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './square.css';
+
+const Square = () => {
+  return(
+      <button className = "square">X</button>
+  )
+}
+
+export default Square
