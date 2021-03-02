653539f60371ff7b162d0156164a22d87d201cd1
StasyTyapkina <stasya.tyapkina@gmail.com>
2021-03-02 14:01:16 +0300

    feat: add fullscreen view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Footer from './components/footer/Footer'
 import Board from './components/board/Board'
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
 
 
 const App = ()=>{
+  const handle = useFullScreenHandle();
+
   return (
     <div className = 'app'>
+
+      <button 
+        className = 'fullScreen_bttn'
+        onClick={handle.enter}
+        title = 'Нажмите, чтобы развернуть игру на весь экран'>
+          <i className = 'fas fa-expand-arrows-alt fullScreen_icon'/>
+      </button>
+
       <h1>Tic-Tac-Toe React Game</h1>
-      <Board/>
+
+      <FullScreen handle={handle}>
+        <Board/>
+      </FullScreen>
+
       <Footer/>
+      
     </div>
   );
 }
